f2a8803f6d268610ab34d72b31a70fb922282843
Aman <akapur07@gmail.com>
2017-08-16 03:04:53 +0530

    ngFor

--- angularEssentailTraining/src/app/app.module.ts
+++ angularEssentailTraining/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.componenet';
-import { mediaItemComponent } from './media/media-item.component'
+import { mediaItemComponent } from './media/media-item.component';
+import { MediaItemListComponent } from './media-item-list/media-item-list.component'
 
 @NgModule({
     imports: [ BrowserModule ],
-    declarations: [ AppComponent, mediaItemComponent],
+    declarations: [ AppComponent, mediaItemComponent, MediaItemListComponent],
     bootstrap: [ AppComponent ]
 })
 
