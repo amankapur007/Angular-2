c6f08403f3e1aa68cb7b255a0de187f1db09c6af
Aman <akapur07@gmail.com>
2017-08-16 00:50:16 +0530

    NgModule, The componenet selector,template, styling, interpolation, property binding, event binding, Output Events(To be continued)

--- angularEssentailTraining/src/app/app.componenet.ts
+++ angularEssentailTraining/src/app/app.componenet.ts
@@ -2,7 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'mw-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls:['./app.component.css']
 })
 
-export class AppComponent {}
+export class AppComponent {
+
+    onMediaItemDelete(mediaItem){
+        
+    }
+
+    firstMediaItem = {
+        name: 'The Redemtion',
+        watchedOn : '13/06/2016',
+        category:'action',
+        year:'2016'
+    }
+}
--- angularEssentailTraining/src/app/app.module.ts
+++ angularEssentailTraining/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.componenet';
+import { mediaItemComponent } from './media/media-item.component'
 
 @NgModule({
     imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent, mediaItemComponent],
     bootstrap: [ AppComponent ]
 })
 
